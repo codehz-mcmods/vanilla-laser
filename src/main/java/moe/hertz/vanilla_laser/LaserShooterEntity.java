add1eb0c3fdde6185cfeb4bee5515b07358ca86b
CodeHz <codehz@outlook.com>
2022-04-17 22:36:12 +0800

    fix laser command

--- src/main/java/moe/hertz/vanilla_laser/LaserShooterEntity.java
+++ src/main/java/moe/hertz/vanilla_laser/LaserShooterEntity.java
@@ -2,11 +2,13 @@ package moe.hertz.vanilla_laser;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
+import org.jetbrains.annotations.Nullable;
+
 import lombok.Getter;
+import lombok.Setter;
 import moe.hertz.vanilla_laser.mixins.GuardianEntityMixin;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
@@ -15,33 +17,24 @@ import net.minecraft.nbt.NbtElement;
 import net.minecraft.network.Packet;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.text.ClickEvent.Action;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.event.listener.EntityGameEventHandler;
 
-public class LaserShooterEntity extends Entity {
-  private class FakeGuardian implements IFakeEntity {
-    @Override
-    public final EntityType<?> getFakeType() {
-      return EntityType.GUARDIAN;
-    }
+public class LaserShooterEntity extends Entity implements IFakeEntity {
+  @Override
+  public EntityType<?> getFakeType() {
+    return EntityType.GUARDIAN;
+  }
 
-    @Getter
-    public final int id = IFakeEntity.generateId();
-    @Getter
-    public final UUID uuid = UUID.randomUUID();
-
-    final Packet<?> data = updateData()
-        .flag(EntityFlag.INVISIBLE)
-        .noGravity(true)
-        .silent(true)
+  Packet<?> getUpdateTarget(int target) {
+    return updateData()
+        .add(GuardianEntityMixin.getTargetTracker(), target)
         .build();
-
-    Packet<?> updateTarget(int target) {
-      return updateData()
-          .add(GuardianEntityMixin.getTargetTracker(), target)
-          .build();
-    }
   }
 
   private static class DummyEntity implements IFakeEntity {
@@ -63,19 +56,35 @@ public class LaserShooterEntity extends Entity {
   }
 
   private final Set<ServerPlayerEntity> trackedPlayers = new HashSet<>();
-  private final FakeGuardian fake = new FakeGuardian();
   private DummyEntity dummy = null;
-  private Optional<UUID> targetEntity = Optional.empty();
-  private Optional<Vec3d> targetPosition = Optional.empty();
+  private @Nullable UUID targetEntity = null;
+  @Getter
+  @Setter
+  private @Nullable Vec3d targetPosition = null;
   private Entity cachedTarget;
   private int cachedTargetId = 0;
   private Packet<?>[] cachedPackets = null;
 
+  public void setTargetEntity(@Nullable Entity entity) {
+    if (entity == null || entity == this) {
+      targetEntity = null;
+    } else {
+      targetEntity = entity.getUuid();
+      cachedTarget = entity;
+    }
+    updateTarget();
+  }
+
+  public @Nullable Entity getTargetEntity() {
+    if (cachedTarget == null || !cachedTarget.getUuid().equals(targetEntity)) {
+      var sw = (ServerWorld) world;
+      cachedTarget = sw.getEntity(targetEntity);
+    }
+    return cachedTarget;
+  }
+
   private final EntityGameEventHandler handler = new EntityGameEventHandler(null) {
     public void onEntityRemoval(World world) {
-      var despawn = fake.getDespawnPacket();
-      for (var player : trackedPlayers)
-        player.networkHandler.sendPacket(despawn);
       if (dummy != null) {
         var dummy_despawn = dummy.getDespawnPacket();
         for (var player : trackedPlayers)
@@ -85,17 +94,20 @@ public class LaserShooterEntity extends Entity {
     };
 
     public void onEntitySetPos(World world) {
-      var move = fake.getMovePacket(getPos());
-      for (var player : trackedPlayers) {
-        player.networkHandler.sendPacket(move);
-      }
     };
   };
 
   public LaserShooterEntity(EntityType<LaserShooterEntity> type, World world) {
     super(type, world);
     setInvulnerable(true);
-    // getId()
+    setInvisible(true);
+    setNoGravity(true);
+    setSilent(true);
+  }
+
+  public LaserShooterEntity(World world, Vec3d position) {
+    this(VanillaLaser.SHOOTER, world);
+    setPosition(position);
   }
 
   @Override
@@ -111,41 +123,38 @@ public class LaserShooterEntity extends Entity {
   protected void readCustomDataFromNbt(NbtCompound nbt) {
     if (nbt.contains("TargetUUID", NbtElement.INT_ARRAY_TYPE)) {
       var uuid = nbt.getUuid("TargetUUID");
-      targetEntity = Optional.of(uuid);
+      targetEntity = uuid;
     } else
-      targetEntity = Optional.empty();
-    var pos = nbt.getCompound("TargetPosition");
-    if (pos != null) {
+      targetEntity = null;
+    if (nbt.contains("TargetPosition", NbtElement.COMPOUND_TYPE)) {
+      var pos = (NbtCompound) nbt.get("TargetPosition");
       var x = pos.getDouble("x");
       var y = pos.getDouble("y");
       var z = pos.getDouble("z");
-      targetPosition = Optional.of(new Vec3d(x, y, z));
+      targetPosition = new Vec3d(x, y, z);
     } else {
-      targetPosition = Optional.empty();
+      targetPosition = null;
     }
     updateTarget();
   }
 
   private void updateTarget() {
-    if (targetEntity.isPresent()) {
-      var targetUuid = targetEntity.get();
-      if (cachedTarget == null || !cachedTarget.getUuid().equals(targetUuid)) {
+    if (targetEntity != null) {
+      if (cachedTarget == null || !cachedTarget.getUuid().equals(targetEntity)) {
         var sw = (ServerWorld) world;
-        cachedTarget = sw.getEntity(targetEntity.get());
+        cachedTarget = sw.getEntity(targetEntity);
       }
-      if (cachedTarget == null) {
-        targetEntity = Optional.empty();
-        updateTarget(0);
-      } else {
+      if (cachedTarget != null) {
         updateTarget(cachedTarget.getId());
+        return;
       }
-    } else if (targetPosition.isPresent()) {
-      var pos = targetPosition.get();
+    }
+    if (targetPosition != null) {
       if (dummy == null) {
         dummy = new DummyEntity();
-        broadcastPacket(dummy.getSpawnPacket(pos), dummy.data);
+        broadcastPacket(dummy.getSpawnPacket(targetPosition), dummy.data);
       } else {
-        broadcastPacket(dummy.getMovePacket(pos));
+        broadcastPacket(dummy.getMovePacket(targetPosition));
       }
       updateTarget(dummy.id);
     } else {
@@ -155,7 +164,7 @@ public class LaserShooterEntity extends Entity {
 
   private void updateTarget(int id) {
     cachedTargetId = id;
-    broadcastPacket(fake.updateTarget(id));
+    broadcastPacket(getUpdateTarget(id));
   }
 
   private void broadcastPacket(Packet<?>... packets) {
@@ -166,21 +175,20 @@ public class LaserShooterEntity extends Entity {
 
   @Override
   protected void writeCustomDataToNbt(NbtCompound nbt) {
-    if (targetEntity.isPresent())
-      nbt.putUuid("TargetUUID", targetEntity.get());
-    if (targetPosition.isPresent()) {
-      var data = targetPosition.get();
+    if (targetEntity != null)
+      nbt.putUuid("TargetUUID", targetEntity);
+    if (targetPosition != null) {
       var pos = new NbtCompound();
-      pos.putDouble("x", data.x);
-      pos.putDouble("y", data.y);
-      pos.putDouble("z", data.z);
+      pos.putDouble("x", targetPosition.x);
+      pos.putDouble("y", targetPosition.y);
+      pos.putDouble("z", targetPosition.z);
       nbt.put("TargetPosition", pos);
     }
   }
 
   @Override
   public Packet<?> createSpawnPacket() {
-    throw new IllegalStateException("Laser shooter should never be sent");
+    return getSpawnPacket(getPos());
   }
 
   @Override
@@ -189,15 +197,13 @@ public class LaserShooterEntity extends Entity {
     var sw = (ServerWorld) world;
     var list = sw.getPlayers();
     if (cachedPackets == null) {
-      var packets = new ArrayList<Packet<?>>(5);
-      packets.add(fake.getSpawnPacket(getPos()));
-      packets.add(fake.data);
+      var packets = new ArrayList<Packet<?>>(3);
       if (dummy != null) {
-        packets.add(dummy.getSpawnPacket(targetPosition.get()));
+        packets.add(dummy.getSpawnPacket(targetPosition));
         packets.add(dummy.data);
       }
       if (cachedTargetId != 0)
-        packets.add(fake.updateTarget(cachedTargetId));
+        packets.add(getUpdateTarget(cachedTargetId));
       cachedPackets = packets.toArray(Packet<?>[]::new);
     }
     for (var player : list) {
@@ -209,5 +215,33 @@ public class LaserShooterEntity extends Entity {
       }
     }
     trackedPlayers.retainAll(list);
+  }
+
+  private static Text dumpVec3d(Vec3d vec) {
+    return Text.of(String.format("(%.2f, %.2f, %.2f)", vec.x, vec.y, vec.z))
+        .copy()
+        .styled(style -> style
+            .withInsertion(String.format("%f %f %f", vec.x, vec.y, vec.z)));
+  }
+
+  @Override
+  public Text getDisplayName() {
+    var txt = LiteralText.EMPTY.copy();
+    txt.append(Text.of("Laser").copy()
+        .styled(style -> style
+            .withHoverEvent(getHoverEvent())
+            .withClickEvent(new ClickEvent(Action.RUN_COMMAND, "/laser edit " + getUuidAsString()))
+            .withInsertion(getUuidAsString())));
+    txt.append(dumpVec3d(getPos()));
+    txt.append(Text.of(" → "));
+    var cached = getTargetEntity();
+    if (cached != null) {
+      txt.append(cached.getDisplayName());
+    } else if (targetPosition != null) {
+      txt.append(dumpVec3d(targetPosition));
+    } else {
+      txt.append(Text.of("(unknown)").copy().styled(style -> style.withStrikethrough(true)));
+    }
+    return txt;
   }
 }
--- src/main/java/moe/hertz/vanilla_laser/VanillaLaser.java
+++ src/main/java/moe/hertz/vanilla_laser/VanillaLaser.java
@@ -1,32 +1,38 @@
 package moe.hertz.vanilla_laser;
 
-import lombok.extern.slf4j.Slf4j;
 import me.lortseam.completeconfig.gui.ConfigScreenBuilder;
 import me.lortseam.completeconfig.gui.cloth.ClothConfigScreenBuilder;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.command.argument.Vec3ArgumentType;
+import net.minecraft.command.argument.*;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.Text;
+import net.minecraft.text.Texts;
+import net.minecraft.text.ClickEvent.Action;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.TypeFilter;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.Registry;
 
 import static net.minecraft.server.command.CommandManager.*;
 
-@Slf4j(topic = "vanilla-laser")
+import java.util.Arrays;
+import java.util.Optional;
+
 public class VanillaLaser implements ModInitializer {
   public static final EntityType<LaserShooterEntity> SHOOTER = Registry.register(
       Registry.ENTITY_TYPE,
       new Identifier("laser", "shooter"),
       FabricEntityTypeBuilder
-          .create(SpawnGroup.CREATURE, LaserShooterEntity::new)
+          .<LaserShooterEntity>create(SpawnGroup.CREATURE, LaserShooterEntity::new)
           .dimensions(EntityDimensions.fixed(0f, 0f))
-          .trackRangeBlocks(0)
           .build());
 
   @Override
@@ -37,41 +43,118 @@ public class VanillaLaser implements ModInitializer {
       ConfigScreenBuilder.setMain("vanilla-laser", new ClothConfigScreenBuilder());
     }
 
-    if (ModConfig.isCommandEnabled())
+    if (ModConfig.isCommandEnabled()) {
+      var cmdname = ModConfig.getCommandName();
       CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
-        dispatcher.register(literal(ModConfig.getCommandName())
-            .then(
-                literal("create")
-                    .then(argument("target", Vec3ArgumentType.vec3())
-                        .executes(context -> {
-                          try {
-                            var src = context.getSource();
-                            var source = src.getPosition();
-                            var target = Vec3ArgumentType.getVec3(context, "target");
-                            createLaser(src.getWorld(), source, target);
-                          } catch (Throwable e) {
-                            log.error("command error", e);
-                          }
-                          return 1;
-                        }).then(argument("source", Vec3ArgumentType.vec3())
-                            .then(argument("target", Vec3ArgumentType.vec3())
-                                .executes(context -> {
-                                  try {
-                                    var src = context.getSource();
-                                    var source = Vec3ArgumentType.getVec3(context, "source");
-                                    var target = Vec3ArgumentType.getVec3(context, "target");
-                                    createLaser(src.getWorld(), source, target);
-                                  } catch (Throwable e) {
-                                    log.error("command error", e);
-                                  }
-                                  return 1;
-                                }))))));
+        var root = literal(cmdname).requires(it -> it.hasPermissionLevel(2));
+        var create = literal("create");
+        create.then(argument("target-location", Vec3ArgumentType.vec3()).executes(ctx -> {
+          var src = ctx.getSource();
+          var source = src.getPosition();
+          var target = Vec3ArgumentType.getVec3(ctx, "target-location");
+          createLaser(src.getWorld(), source, target);
+          return 1;
+        }));
+        create.then(argument("target-entity", EntityArgumentType.entity()).executes(ctx -> {
+          var src = ctx.getSource();
+          var source = src.getPosition();
+          var target = EntityArgumentType.getEntity(ctx, "target-entity");
+          createLaser(src.getWorld(), source, target);
+          return 1;
+        }));
+        var list = literal("list").executes(ctx -> {
+          var src = ctx.getSource();
+          var world = src.getWorld();
+          var entities = world.getEntitiesByType(TypeFilter.instanceOf(LaserShooterEntity.class), it -> true);
+          src.sendFeedback(Texts.join(entities, Optional.of(Text.of("\n")), Entity::getDisplayName), false);
+          return 1;
+        });
+        var editUuid = argument("uuid", UuidArgumentType.uuid());
+        editUuid.executes(ctx -> {
+          var src = ctx.getSource();
+          var world = src.getWorld();
+          var uuid = UuidArgumentType.getUuid(ctx, "uuid");
+          var entity = world.getEntity(uuid);
+          if (entity != null && entity instanceof LaserShooterEntity shooter) {
+            var tp = Text.of("teleport").copy().styled(style -> style
+                .withUnderline(true)
+                .withClickEvent(
+                    new ClickEvent(Action.SUGGEST_COMMAND, "/tp " + uuid.toString())));
+            var retarget = Text.of("set target").copy().styled(style -> style
+                .withUnderline(true)
+                .withClickEvent(
+                    new ClickEvent(Action.SUGGEST_COMMAND,
+                        String.format("/%s edit %s set", cmdname, uuid))));
+            var kill = Text.of("kill").copy().styled(style -> style
+                .withUnderline(true)
+                .withClickEvent(
+                    new ClickEvent(Action.RUN_COMMAND, "/kill " + uuid.toString())));
+            src.sendFeedback(Texts.join(Arrays.asList(tp, retarget, kill), Text.of("  ")), false);
+            return 1;
+          } else {
+            src.sendError(Text.of("invalid uuid"));
+            return 0;
+          }
+        });
+        var editSet = literal("set").executes(ctx -> {
+          var src = ctx.getSource();
+          var world = src.getWorld();
+          var uuid = UuidArgumentType.getUuid(ctx, "uuid");
+          var entity = world.getEntity(uuid);
+          if (entity != null && entity instanceof LaserShooterEntity shooter) {
+            shooter.setTargetPosition(src.getPosition());
+            shooter.setTargetEntity(src.getPlayer());
+          } else {
+            src.sendError(Text.of("invalid uuid"));
+            return 0;
+          }
+          return 1;
+        }).then(argument("new-location", Vec3ArgumentType.vec3()).executes(ctx -> {
+          var src = ctx.getSource();
+          var world = src.getWorld();
+          var uuid = UuidArgumentType.getUuid(ctx, "uuid");
+          var target = Vec3ArgumentType.getVec3(ctx, "new-location");
+          var entity = world.getEntity(uuid);
+          if (entity != null && entity instanceof LaserShooterEntity shooter) {
+            shooter.setTargetPosition(target);
+            shooter.setTargetEntity(null);
+          } else {
+            src.sendError(Text.of("invalid uuid"));
+            return 0;
+          }
+          return 1;
+        })).then(argument("new-entity", EntityArgumentType.entity()).executes(ctx -> {
+          var src = ctx.getSource();
+          var world = src.getWorld();
+          var uuid = UuidArgumentType.getUuid(ctx, "uuid");
+          var target = EntityArgumentType.getEntity(ctx, "new-entity");
+          var entity = world.getEntity(uuid);
+          if (entity != null && entity instanceof LaserShooterEntity shooter) {
+            shooter.setTargetEntity(target);
+          } else {
+            src.sendError(Text.of("invalid uuid"));
+            return 0;
+          }
+          return 1;
+        }));
+        editUuid.then(editSet);
+        var edit = literal("edit").then(editUuid);
+        dispatcher.register(root.then(create).then(list).then(edit));
       });
+    }
   }
 
-  public static LaserInstance createLaser(ServerWorld world, Vec3d source, Vec3d target) {
-    var ins = new LaserInstance(source, target);
-    ins.spawn(world);
-    return ins;
+  public static LaserShooterEntity createLaser(ServerWorld world, Vec3d source, Vec3d target) {
+    var entity = new LaserShooterEntity(world, source);
+    entity.setTargetPosition(target);
+    world.spawnEntity(entity);
+    return entity;
+  }
+
+  public static LaserShooterEntity createLaser(ServerWorld world, Vec3d source, Entity target) {
+    var entity = new LaserShooterEntity(world, source);
+    entity.setTargetEntity(target);
+    world.spawnEntity(entity);
+    return entity;
   }
 }
